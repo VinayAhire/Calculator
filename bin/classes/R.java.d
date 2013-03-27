451bbc01de35a9cdecade064b0b2d9bf8aa7f212
Vinay Ahire <vinaya@xpanxion.co.in>
2013-03-27 13:03:04 +0530

    After new build.

---
+++ bin/classes/R.java.d
@@ -0,0 +1,7 @@
+C:\Users\vinaya\21March\AndroidCalculator\gen\com\calculator\R.java \
+ : C:\Users\vinaya\21March\AndroidCalculator\res\drawable-hdpi\icon.png \
+C:\Users\vinaya\21March\AndroidCalculator\res\drawable-ldpi\icon.png \
+C:\Users\vinaya\21March\AndroidCalculator\res\drawable-mdpi\icon.png \
+C:\Users\vinaya\21March\AndroidCalculator\res\layout\main.xml \
+C:\Users\vinaya\21March\AndroidCalculator\res\values\strings.xml \
+C:\Users\vinaya\21March\AndroidCalculator\bin\AndroidManifest.xml \
